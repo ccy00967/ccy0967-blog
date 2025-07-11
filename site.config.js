c208b5367cec83a49ad2233399af6b6e9a76f966
Chulyoung <ccy09671324@gmail.com>
2025-07-12 00:21:57 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   link: "https://ccy0967-blog.vercel.app/",
   since: 2025, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://github.com/ccy00967.png", // The link to generate OG image, don't end with a slash
   
   // notion configuration (required)
   notionConfig: {
