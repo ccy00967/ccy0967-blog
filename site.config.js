4c1f73746355c1e3073316bb23fe3793da0c95ff
chulyoung <ccy09671324@gmail.com>
2025-07-12 09:59:18 +0900

    fix

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Chae, Chul Young",
-    image: "/wallpaperbetter.com_3024x1964.jpg",
+    image: "/avatar.svg",
     role: "student",
     bio: "I believe in Think & Grow Rich",
     email: "ccy09671324@gmail.com",
