c05790983f8464cc8d86463282b6ce5f199db960
chulyoung <ccy09671324@gmail.com>
2025-09-01 19:12:49 +0900

    delete blank

--- src/routes/Detail/PostDetail/CommentBox/Utterances.tsx
+++ src/routes/Detail/PostDetail/CommentBox/Utterances.tsx
@@ -1,54 +1,54 @@
+import React, { useEffect, useRef } from "react"
 import { CONFIG } from "site.config"
-import { useEffect } from "react"
-import styled from "@emotion/styled"
 import useScheme from "src/hooks/useScheme"
-import { useRouter } from "next/router"
-
-//TODO: useRef?
+import styled from "@emotion/styled"
 
 type Props = {
   issueTerm: string
 }
 
 const Utterances: React.FC<Props> = ({ issueTerm }) => {
+  const ref = useRef<HTMLDivElement>(null)
   const [scheme] = useScheme()
-  const router = useRouter()
 
   useEffect(() => {
+    if (!ref.current) return
+
     const theme = `github-${scheme}`
     const script = document.createElement("script")
-    const anchor = document.getElementById("comments")
-    if (!anchor) return
 
-    script.setAttribute("src", "https://utteranc.es/client.js")
-    script.setAttribute("crossorigin", "anonymous")
-    script.setAttribute("async", `true`)
-    script.setAttribute("issue-term", issueTerm)
-    script.setAttribute("theme", theme)
-    const config: Record<string, string> = CONFIG.utterances.config
-    Object.keys(config).forEach((key) => {
-      script.setAttribute(key, config[key])
-    })
-    anchor.appendChild(script)
-    return () => {
-      anchor.innerHTML = ""
+    const config: Record<string, string> = {
+      src: "https://utteranc.es/client.js",
+      repo: CONFIG.utterances.config.repo,
+      "issue-term": issueTerm,
+      label: "💬 Utterances",
+      theme,
+      crossorigin: "anonymous",
+      async: "true",
     }
-  }, [scheme, router])
-  return (
-    <>
-      <StyledWrapper id="comments">
-        <div className="utterances-frame"></div>
-      </StyledWrapper>
-    </>
-  )
+
+    Object.entries(config).forEach(([key, value]) => {
+      script.setAttribute(key, value)
+    })
+
+    ref.current.appendChild(script)
+
+    // Clean up
+    return () => {
+      if (ref.current) {
+        ref.current.innerHTML = ""
+      }
+    }
+  }, [scheme, issueTerm]) // Rerun effect when theme or post changes
+
+  return <StyledWrapper ref={ref} />
 }
 
 export default Utterances
 
 const StyledWrapper = styled.div`
-  min-height: 200px;
-
-  @media (min-width: 768px) {
-    margin-left: -4rem;
+  width: 100%;
+  .utterances {
+    max-width: 100% !important;
   }
 `
